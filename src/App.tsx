96aa8284fb68f827bf2b0d489efe52e506964ff7
Grim's Desktop <paulperson.pro@gmail.com>
2021-05-11 23:20:22 +0200

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,96 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Formik, Form, Field } from "formik"
+import { useEffect, useState } from "react"
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface CreditCardData {
+  number: {
+    value: string
+    error: string | null
+  }
+  name: string
+  month: string
+  year: string
+  ccv: string
 }
 
-export default App;
+const Input = ({
+  label,
+  ...props
+}: {
+  label: string
+  name: string
+  type: string
+  onChange(e: any): void
+}) => {
+  return (
+    <div className="Input-wrapper">
+      <input {...props} className="Input" />
+      <label className="Input-label">{label}</label>
+    </div>
+  )
+}
+
+function App() {
+  const [data, setData] = useState<CreditCardData>({
+    number: {
+      value: "",
+      error: null,
+    },
+    name: "",
+    month: "",
+    year: "",
+    ccv: "",
+  })
+
+  useEffect(() => {
+    console.log(data)
+  }, [data])
+
+  return (
+    <div className="app">
+      <div className="wrapper">
+        <div className="credit-card">
+          <img
+            src="https://raw.githubusercontent.com/muhammederdem/credit-card-form/master/src/assets/images/2.jpeg"
+            alt="credit-card"
+            className="card"
+          />
+          <img
+            src="https://raw.githubusercontent.com/muhammederdem/credit-card-form/master/src/assets/images/chip.png"
+            alt="chip"
+            className="card-chip"
+          />
+        </div>
+        <form className="form">
+          <Input
+            name="number"
+            label="Card Number"
+            type="text"
+            onChange={(e: any) =>
+              setData({
+                ...data,
+                number: { ...data.number, value: e.target.value },
+              })
+            }
+          />
+          <Input
+            name="name"
+            label="Card Name"
+            type="text"
+            onChange={(e) => setData({ ...data, name: e.target.value })}
+          />
+          <div className="flex">
+            <div className="column">
+              <label htmlFor="month">Expiration Date</label>
+              <select name="" id="" className=""></select>
+            </div>
+            <select name="" id="" className="column"></select>
+            <input type="text" className="column" />
+          </div>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default App
